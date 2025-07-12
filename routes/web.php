627d40645e93ac03a368b125873256416aea950c
minhnguyenparalax <nguyenquang123uvb@gmail.com>
2025-07-12 10:41:07 +0700

    Fix route lỗi trích xuất biến

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ExcelController;
 use App\Http\Controllers\DocController;
+use App\Http\Controllers\ExcelDocMappingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ExcelController::class, 'viewExcelFiles'])->name('file.index');
@@ -18,4 +19,5 @@ Route::post('/doc/remove', [DocController::class, 'removeDoc'])->name('doc.remov
 Route::get('/doc/read/{docId}', [DocController::class, 'readDoc'])->name('doc.readDoc');
 Route::post('/excel/create-table-and-insert-data/{fileId}/{sheetId}', [ExcelController::class, 'createTableAndInsertData'])->name('excel.createTableAndInsertData');
 Route::get('/doc/select/{docId}', [App\Http\Controllers\DocController::class, 'selectDoc'])->name('doc.selectDoc');
+Route::get('/mapping/fields/{variableId}', [ExcelDocMappingController::class, 'getFields'])->name('excel_doc_mapping.getFields');
 
